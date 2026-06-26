1c02d7846a7dc738b59b913d7fb17cca9a3431cd
Daniel Steven Lopez Perez <dast11lp@gmail.com>
2026-06-25 23:00:36 -0500

    formulario con validariones y candinary

--- admin/admin.js
+++ admin/admin.js
@@ -1,8 +1,15 @@
 // inicio formulario daniel
 const formularioVideo = document.querySelector("#formulario_video");
+
 const selectCategoria = document.querySelector("#category")
+const inputURL = document.querySelector("#videoUrl");
+const descriptionArea = document.querySelector("#description");
+
+
 const btnEnviarFormulario = document.querySelector("#send");
-const videoURL = document.querySelector("#videoUrl");
+
+const spanError = document.querySelectorAll('.error')
+
 
 const tabSeccion = document.querySelector('.tabs')
 const tabList = document.querySelectorAll('.tabs__list__tab')
@@ -16,33 +23,137 @@ tabSeccion.addEventListener('click', ({ target }) => {
     const tab = target.closest('.tabs__list__tab');
     if (!tab) return;
 
-    tabList.forEach((el)=> {
+    tabList.forEach((el) => {
         el.classList.remove("tabs__list__tab--active")
     });
     target.classList.add("tabs__list__tab--active");
-    
-    tabContent.forEach((el)=> {
+
+    tabContent.forEach((el) => {
         el.classList.remove("tabs__content--active")
     })
     document.querySelector(`.tabs__content--${tab.dataset.tab}`).classList.add("tabs__content--active");
 })
 
+// capturar video 
+
+const subirVideoCloudinary = async (archivo) => {
+    try {
+        if (!archivo) {
+            console.log("inserta un archivo de video");
+            return
+        }
+
+        const tipoArchivo = archivo.type.split("/")[0]
+
+        if (tipoArchivo !== 'video') {
+            console.log("por favor ingresar un archivo de video");
+            return
+        }
+
+        const formData = new FormData();
+        formData.append("file", archivo);
+        formData.append("upload_preset", "ml_default");
+        formData.append("folder", "alumni/cursos/")
+
+        const respuesta = await fetch("https://api.cloudinary.com/v1_1/dd9iztlrv/video/upload", {
+            method: "POST",
+            body: formData
+        })
 
 
-btnEnviarFormulario.addEventListener('click', (e) => {
+        if (!respuesta.ok) {
+            console.log("Error al subir el video, intenta de nuevo");
+            return
+        }
+
+        const datos = await respuesta.json();
+        return datos.secure_url;
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// validar formulario
+
+const camposFormulario = [
+    {
+        input: selectCategoria,
+        validaciones: [
+            { validar: (valor) => valor !== "", mensaje: "selecciona una categoría" },
+        ],
+
+    },
+    {
+        input: descriptionArea,
+        validaciones: [
+            { validar: (valor) => valor !== "", mensaje: "Por favor agregar una descripción", },
+        ]
+    }
+]
+
+const mostrarError = (input, mensaje) => {
+    input.nextElementSibling.textContent = mensaje;
+}
+
+function limpiarError(input) {
+    input.nextElementSibling.textContent = "";
+}
+
+
+btnEnviarFormulario.addEventListener('click', async (e) => {
 
     e.preventDefault();
-    const urlTest = regexURL.test(videoURL.value);
-    if (selectCategoria.value === "") {
-        console.log("debes seleccionar una categoría");
-        return
-    }
-    if (!urlTest) {
-        console.log("la url no es válida");
+    let valido = true;
+
+    const tabActiva = document.querySelector(".tabs__list__tab--active").dataset.tab;
+
+    let urlFInal = "";
+
+    if (tabActiva === "1") {
+        const archivoVideo = document.querySelector("#videoFile").files[0];
+        urlFInal = await subirVideoCloudinary(archivoVideo)
+    } else {
+        if (inputURL.value === "") {
+            mostrarError(inputURL, "campo vacío")
+            valido = false;
+
+        } else if (!regexURL.test(inputURL.value.trim())) {
+            mostrarError(inputURL, "ingresa una url válida");
+            valido = false;
+        } else {
+            urlFInal = inputURL.value;
+        }
+
     }
 
-    const datosFormulario = new FormData(formularioVideo);
-    console.log(datosFormulario);
+
+    camposFormulario.forEach(campo => {
+        limpiarError(campo.input);
+        for (const validacion of campo.validaciones) {
+            if (!validacion.validar(campo.input.value)) {
+                mostrarError(campo.input, validacion.mensaje);
+                valido = false;
+                break;
+            }
+        }
+    });
+
+    if (!valido) return
+
+    const nuevoVideo = {
+        url: urlFInal,
+        categoria: selectCategoria.value,
+        descripcion: descriptionArea.value
+    }
+
+    console.log(nuevoVideo);
+    
+
+    localStorage.setItem("video", JSON.stringify(nuevoVideo))
+
+
 })
 
 // fin  formulario daniel
@@ -80,26 +191,26 @@ const webinars = [
 ];
 
 const historias = [
-  {
-    nombre: "Juan Rodríguez",
-    empresa: "Teleperformance",
-    tiempo: "2 meses",
-    testimonio:
-      "La sección de Networking fue clave. Conecté con un reclutador y a las dos semanas tenía trabajo.",
-    rol: "IT Support",
-    anio: "2023",
-    foto: "https://randomuser.me/api/portraits/men/32.jpg"
-  },
-  {
-    nombre: "María López",
-    empresa: "Mercado Libre",
-    tiempo: "4 meses",
-    testimonio:
-      "Gracias a las simulaciones de entrevista conseguí mi primer empleo.",
-    rol: "Frontend Developer",
-    anio: "2024",
-    foto: "https://randomuser.me/api/portraits/women/44.jpg"
-  }
+    {
+        nombre: "Juan Rodríguez",
+        empresa: "Teleperformance",
+        tiempo: "2 meses",
+        testimonio:
+            "La sección de Networking fue clave. Conecté con un reclutador y a las dos semanas tenía trabajo.",
+        rol: "IT Support",
+        anio: "2023",
+        foto: "https://randomuser.me/api/portraits/men/32.jpg"
+    },
+    {
+        nombre: "María López",
+        empresa: "Mercado Libre",
+        tiempo: "4 meses",
+        testimonio:
+            "Gracias a las simulaciones de entrevista conseguí mi primer empleo.",
+        rol: "Frontend Developer",
+        anio: "2024",
+        foto: "https://randomuser.me/api/portraits/women/44.jpg"
+    }
 ];
 
 function mostrarWebinars() {
@@ -142,10 +253,10 @@ function mostrarWebinars() {
 }
 
 function mostrarHistorias() {
-  const contenedor = document.getElementById("tarjetas-historias");
+    const contenedor = document.getElementById("tarjetas-historias");
 
-  historias.forEach(historia => {
-    contenedor.innerHTML += `
+    historias.forEach(historia => {
+        contenedor.innerHTML += `
       <div class="card-historia">
 
         <div class="header-historia">
@@ -180,7 +291,7 @@ function mostrarHistorias() {
 
       </div>
     `;
-  });
+    });
 }
 
 mostrarWebinars();
@@ -196,11 +307,11 @@ function buscarFiltrarGrabaciones() {
     const inputBusqueda = document.getElementById('busqueda-sesiones');
     console.log("El botón funciona");
     console.log(inputBusqueda.value);
-    
+
     const textoDigitado = inputBusqueda.value.trim().toLowerCase();
 
     if (textoDigitado != "") {
-        const grabacionesEncontradas = listraGrabaciones.filter(grabacion => 
+        const grabacionesEncontradas = listraGrabaciones.filter(grabacion =>
             grabacion.nombre?.toLowerCase().includes(textoDigitado));
         crearTarjetaGrabacionEncontrada(grabacionesEncontradas);
     } else {
