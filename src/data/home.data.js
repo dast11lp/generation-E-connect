29d19950f74055b67c39d4bde42a97fea6683849
MariaGongora <margongora2004@gmail.com>
2026-08-05 09:56:05 -0500

    Cambios finales

--- src/data/home.data.js
+++ src/data/home.data.js
@@ -21,5 +21,4 @@ export const homeStats = [
   { id: "categorias", numero: "8", etiqueta: "Categorías" },
   { id: "grabaciones", numero: "12", etiqueta: "Grabaciones" },
   { id: "programas", numero: "4", etiqueta: "Programas" },
-  { id: "alumnos", numero: "200+", etiqueta: "Alumnos activos" },
 ];
